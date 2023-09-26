2421439738b94a74763ef5a3bbc0e640c4467db2
Britta <kappe619@gmail.com>
2023-09-26 13:27:08 +0200

    Add cost calculation in CalculatorView

--- SppdCalculator/CalculateView.swift
+++ SppdCalculator/CalculateView.swift
@@ -12,31 +12,73 @@ struct CalculateView: View {
     let rarities: [Rarity] = [.Common, .Rare, .Epic, .Legendary]
     static let defaultLevel = 4
     static let defaultRarity = Rarity.Common
+    static let defaultCosts = 0
     
-    @Binding var selectedRarity: Rarity
-    @Binding var selectedLevel: Int
-   
+    let commonCosts =       [0, 0, 0, 0, 100, 275, 575]
+    let rareCosts =         [0, 0, 0, 125, 325, 650, 1075]
+    let epicCosts =         [0, 0, 175, 425, 800, 1275, 1825]
+    let legendaryCosts =    [0, 200, 475, 875, 1375, 1950, 2575]
+    
+    @State var selectedRarity: Rarity
+    @State var selectedLevel: Int
+    @Binding var costs: Int
+    @State var t = ""
     
     var body: some View {
         VStack {
-            TextEditor(text: /*@START_MENU_TOKEN@*/.constant("Placeholder")/*@END_MENU_TOKEN@*/)
+            TextEditor(text: $t)
             
+                .textFieldStyle((RoundedBorderTextFieldStyle()))
+                .frame(width: 200, height: 70)
+                .font(.title)
+
             HStack{
+                
+                
                 Picker("", selection: $selectedRarity){
                     ForEach(rarities, id: \.self) {rarity in Text(rarity.rawValue).tag(rarity)
-                        
                     }
+                    .frame(width: 70)
                 }
-                Stepper("", value: $selectedLevel, in: 1...7)
+                Stepper("\(selectedLevel)", value: $selectedLevel, in: 1...7)
                     .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                    .font(.system(size: 20))
+                    .font(.system(size: 15))
+                    .onChange(of: selectedLevel) { newValue in
+                        self.costs = calcCosts() }
             }
-            Text("calculated costs")
+                
+
+                .onChange(of: selectedRarity) { newValue in
+                    self.costs = calcCosts() }
+            Text("costs: \(costs)")
+                .font(.system(size: 15))
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+
         }
     }
     
+    
+    func calcCosts() -> Int {
+        var temp = 0
+        let index = selectedLevel - 1
+        
+        switch selectedRarity {
+        case .Common:
+            temp = commonCosts[index]
+        case .Rare:
+            temp = rareCosts[index]
+        case .Epic:
+            temp = epicCosts[index]
+        case .Legendary:
+            temp = legendaryCosts[index]
+        }
+        return temp
+    }
+    
+
 }
 
-#Preview {
-    CalculateView(selectedRarity: .constant(CalculateView.defaultRarity), selectedLevel: .constant(CalculateView.defaultLevel))
-}
+//#Preview {
+//    CalculateView(selectedRarity: /*.constant(CalculateView.defaultRarity), selectedLevel: .constant(CalculateView.defaultLevel),*/ costs: .constant(CalculateView.defaultCosts))
+//        
+//}
--- SppdCalculator/ContentView.swift
+++ SppdCalculator/ContentView.swift
@@ -9,25 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var selectedLevel1: Int = CalculateView.defaultLevel
-    @State private var selectedRarity1: Rarity = CalculateView.defaultRarity
+    // @State private var selectedLevel1: Int = CalculateView.defaultLevel
+    // @State private var selectedRarity1: Rarity = CalculateView.defaultRarity
+    @State private var costs1: Int = 0
+    @State private var costs2: Int = 0
+    @State private var costs3: Int = 0
+    @State private var costs4: Int = 0
+    @State private var costs5: Int = 0
+    @State private var costs6: Int = 0
+    @State private var costs7: Int = 0
+    @State private var costs8: Int = 0
+    @State private var costs9: Int = 0
+    @State private var costs10: Int = 0
+    @State private var costs11: Int = 0
+    @State private var costs12: Int = 0
     
+    var totalCosts: Int{
+        return costs1 + costs2 + costs3 + costs4 + costs5 + costs6 + costs7 + costs8 + costs9 + costs10 + costs11 + costs12
+    }
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            CalculateView(selectedRarity: $selectedRarity1, selectedLevel: $selectedLevel1)
+        
+
+            VStack {
+                
+                
+                HStack{
+                    
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs1)
+                    
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs2)
+                    
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs3)
+                }
+                
+                HStack{
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs4)
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs5)
+                    
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs6)
+                }
+                
+                HStack{
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs7)
+                    
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs8)
+                    
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs9)
+                }
+                
+                HStack{
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs10)
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs11)
+                    CalculateView(selectedRarity: CalculateView.defaultRarity, selectedLevel: CalculateView.defaultLevel, costs: $costs12)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
-}
-
-
-
-
 
 
 #Preview {
